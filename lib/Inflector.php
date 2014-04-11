52dd69beb2ca6a82aa4c2a05104e50d02bba83ed
Koen Punt <koen@koenpunt.nl>
2014-04-11 16:15:08 +0200

    transliterate using iconv and custom transliterate table

--- lib/Inflector.php
+++ lib/Inflector.php
@@ -354,10 +354,14 @@ class Inflector{
 	 * @author Koen Punt
 	 */
 	public static function transliterate($string, $replacement = "?"){
-		if(!function_exists('transliterator_transliterate')){
-			trigger_error('PHP Internationalization extension not installed.', E_USER_ERROR);
+		if(!function_exists('normalizer_normalize') || !function_exists('iconv')){
+			trigger_error('PHP Internationalization and/or iconv extension not installed.', E_USER_ERROR);
 		}
-		return transliterator_transliterate('Any-Latin; Latin-ASCII', $string);
+		$roman = array('Sch','sch','Yo','Zh','Kh','Ts','Ch','Sh','Yu','ya','yo','zh','kh','ts','ch','sh','u','a','A','B','V','G','D','E','Z','I','Y','K','L','M','N','O','P','R','S','T','U','F','','Y','','E','a','b','v','g','d','e','z','i','y','k','l','m','n','o','p','r','s','t','u','f','','y','','e');
+		$cyrillic = array('Щ','щ','Ё','Ж','Х','Ц','Ч','Ш','Ю','я','ё','ж','х','ц','ч','ш','ю','я','А','Б','В','Г','Д','Е','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Ь','Ы','Ъ','Э','а','б','в','г','д','е','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','ь','ы','ъ','э');
+		$string = normalizer_normalize($string, \Normalizer::FORM_D);
+		$string = strtr($string, array_combine($cyrillic, $roman));
+		return iconv('UTF-8', 'ASCII//IGNORE//TRANSLIT', $string);
 	}
 
 	/**
